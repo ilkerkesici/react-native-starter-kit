75361ab47efbe688f12ca97b836fa630c3daba20
İlker Kesici <ilkerrkesici@gmail.com>
2020-06-15 16:17:23 +0300

    feat: replace creative files

--- create_app.sh
+++ create_app.sh
@@ -8,11 +8,11 @@ echo "Enter js for Javascript templates, ts for Typescript templates : "
 read type
 
 if [ $type = "js" ]; then
-    chmod +x ./create_js_app.sh # Give permission for execute
-    ./create_js_app.sh $app
+    chmod +x ./src/create_js_app.sh # Give permission for execute
+    ./src/create_js_app.sh $app
 else                                  # default ts
-    chmod +x ./create_ts_app.sh  # Give permission for execute
-    ./create_ts_app.sh $app
+    chmod +x ./src/create_ts_app.sh  # Give permission for execute
+    ./src/create_ts_app.sh $app
 fi;
 
 
